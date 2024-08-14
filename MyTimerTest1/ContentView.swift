cc4e028d2456db9ce58863351723ed1f9b501267
morisum <moris@r.sannet.ne.jp>
2024-08-15 08:59:33 +0900

    Initial Commit

---
+++ MyTimerTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTimerTest1
+//
+//  Created by MsMacM on 2024/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTimerTest1/MyTimerTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerTest1App.swift
+//  MyTimerTest1
+//
+//  Created by MsMacM on 2024/08/15.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
