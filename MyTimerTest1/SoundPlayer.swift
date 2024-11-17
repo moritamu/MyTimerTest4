72a69d1691e0109a43c2ffa703dcbf8a5bb9e2c7
morisum <moris@r.sannet.ne.jp>
2024-11-17 16:36:56 +0900

    SoundPlayerのファイルを追加してClassを作る

---
+++ MyTimerTest1/SoundPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  SoundPlayer.swift
+//  MyTimerTest1
+//
+//  Created by MsMacM on 2024/11/17.
+//
+
+import UIKit
+import AVFoundation
+
+
+class SoundPlayer: NSObject {
+    let musicData = NSDataAsset(name: "kettei")!.data
+    
+    var player: AVAudioPlayer!
+    
+    func play() {
+        player = try! AVAudioPlayer(data: musicData)
+        player.play()
+    }
+
+}
